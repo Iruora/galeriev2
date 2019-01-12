fa12af5112fc653984d901f269da9c9a39e93f63
Nidhal Arouri <arrouurii@gmail.com>
2019-01-12 07:54:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,9 +15,13 @@
 
 
 <body>
+	<?php
+//include the S3 class              
+ 
+//we'll continue our script from here in step 4!
+ 
+?>
 <?php
-
-
  if(isset($_GET['add'])){if($_GET['add']==0){echo "<div class='alert alert-success alert-dismissible'>
    <a href='#'' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
    <strong>Success!</strong> Your image has been successfully added !
@@ -25,13 +29,11 @@
    <a href='#'' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
    <strong>Echec!</strong> Error while adding your image !
  </div>";}}
-
-
   ?>
 <br><br>
 <div class="container">
     <div class="row">
-        
+        <img src="./assets/N.png" alt="logo" width="100" height="100" class="col-md-offset-5">
     </div>
 	<div class="row">
 		<form action="" method="POST" enctype="multipart/form-data" class="col-sm-4" style="margin-left:30%" >
@@ -48,13 +50,12 @@
 	</div>
 </div>
 <?php
-	//include the S3 class              
-	if (!class_exists('S3'))require_once('S3.php');
+	//include the S3 class             
 	try
 	{
-		$param='mysql:host=mydb.c2ewwi1jafca.eu-west-1.rds.amazonaws.com;dbname=galerie';
+		$param='mysql:host=mydb.cziozwt7vuhe.eu-west-1.rds.amazonaws.com;dbname=galerie';
 		$user='root';
-		$password='rootroot';
+		$password='';
 		$bd=new PDO($param,$user,$password);
 		$bd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         $table = 'images';
@@ -63,7 +64,7 @@
 	{
 		echo " Database error".$Obj->getMessage();
 	}
-    if(isset($_POST['submit'])){ 
+    	if(isset($_POST['submit'])){ 
 			$nomimg = $_FILES['img']['name'] ;
 			$nomimg = str_replace(' ', '', $nomimg);
 			$nomimg = str_replace('é', 'e', $nomimg);
@@ -73,23 +74,31 @@
 			$nomimg = str_replace('-', '_', $nomimg);
 			$nomimg = str_replace('/', '_', $nomimg);
 			$nomimg = str_replace('@', '_', $nomimg);
-			$tmpfilename = $_FILES['img']['tmp_name'] ;
-	    		$s3->putBucket("sourours3", S3::ACL_PUBLIC_READ);
+	    
+	    		$tmpfilename = $_FILES['img']['tmp_name'] ;
+	                if (!class_exists('S3'))require_once('S3.php');
+ 
+			//AWS access info
+			if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJN6T756MUGUJIVHA');
+			if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+ 
+			//instantiate the class
+			$s3 = new S3(awsAccessKey, awsSecretKey);
+	    		//var_dump($s3->putBucket("sourours3", S3::ACL_PUBLIC_READ));
 			//move the file
 			if ($s3->putObjectFile($tmpfilename, "sourours3", $nomimg, S3::ACL_PUBLIC_READ)) {
 				echo "<strong>S3::We successfully uploaded your file.</strong>";
 			}else{
 				echo "<strong>S3::Something went wrong while uploading your file... sorry.</strong>";
 			}
-            $sql=$bd->prepare("INSERT INTO `images` (`name`,`src`) VALUES (:src,:src)");
+            		////////////////////////////////////////////////////////////
+	    		$sql=$bd->prepare("INSERT INTO `images`(`name`, `src`) VALUES (:src,:src)");
+	    
 			$sql->bindParam('src', $nomimg);
 			$res=$sql->execute();
-
 			$dirpath = realpath(dirname(getcwd()));
 			$document=$dirpath."/./uploads/".$nomimg;
-
 			$result = move_uploaded_file($_FILES['img']['tmp_name'],$document);
-
 			if ($res){ echo "Success";
 				header('location:./index.php?add=0'); }
 			else header('location:./index.php?add=1');
